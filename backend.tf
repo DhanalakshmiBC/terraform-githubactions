a709b35f23b1d403f29135889e57b0c02ccdf269
Deepika <deepika@Deepikas-MacBook-Air.local>
2025-07-08 17:48:44 +0530

    heh

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 
 terraform {
   backend "s3" {
-    bucket         = "my-terraform-state-bobby-aaa"
+    bucket         = "my-terraform-state-bucket-aaa"
     region         = "us-east-1"
     key            = "s3-github-actions/terraform.tfstate"
     encrypt = true
